f8644579f8dec846602a50e617c52e584f46d599
ggertzog <musienko.roma@mail.ru>
2025-01-15 20:10:22 +0300

    initializing and configuring the project

---
+++ app/layout.tsx
@@ -0,0 +1,27 @@
+import type { Metadata } from 'next';
+import { Nunito } from 'next/font/google';
+import './globals.css';
+
+const nunito = Nunito({
+  subsets: ['cyrillic'],
+  variable: '--font-nunito',
+  weight: ['400', '500', '600', '700', '800', '900'],
+});
+
+export const metadata: Metadata = {
+  title: 'Next Pizza Store',
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={`${nunito.variable}`}>
+        <main className="min-h-screen">{children}</main>
+      </body>
+    </html>
+  );
+}
---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+import { Button } from '@/components/ui/button';
+import Image from 'next/image';
+
+export default function Home() {
+  return <h1>Home
+    <Button variant="outline">Корзина</Button>
+  </h1>;
+}
